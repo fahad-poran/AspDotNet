a4a7cdee6d5d5c6dc5271d38b91f6b0439d09ec8
fahad-poran <55590627+fahad-poran@users.noreply.github.com>
2021-10-02 17:48:07 +0600

    Practice code

---
+++ testing/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testing.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+    //    public ActionResult Index()
+    //    {
+    //        return View();
+    //    }
+
+    public ActionResult List()
+        {
+            ViewBag.Name = "fahad poran";
+            string[] names = new string[3];
+            names[0] = "jakir";
+            names[1] = "jakir";
+            names[2] = "jakir";
+
+            ViewBag.names = names;
+            return View();
+            //person/list
+        }
+
+    }
+}
---
+++ testing/Controllers/studentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testing.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        public ActionResult Person()
+        {
+            return View();
+        }
+    }
+}
